1ad637b69b579fa3338cf93a2b041a11b9f17bda
tugalsan karabacak <tugalsan@users.noreply.github.com>
2023-10-19 13:16:50 +0300

    .

---
+++ src/main/java/com/tugalsan/gvm/screensize/TS_GVMFixScreenSizeUtils.java
@@ -0,0 +1,46 @@
+package com.tugalsan.gvm.screensize;
+
+import com.tugalsan.api.cast.client.TGS_CastUtils;
+import com.tugalsan.api.file.server.TS_FileUtils;
+import com.tugalsan.api.file.txt.server.TS_FileTxtUtils;
+import com.tugalsan.api.optional.client.TGS_Optional;
+import com.tugalsan.api.os.server.TS_OsProcess;
+import com.tugalsan.api.stream.client.TGS_StreamUtils;
+import com.tugalsan.api.string.client.TGS_StringUtils;
+import com.tugalsan.api.string.server.TS_StringUtils;
+import java.awt.Rectangle;
+import java.nio.file.FileSystems;
+
+public class TS_GVMFixScreenSizeUtils {
+
+    public static TGS_Optional<Rectangle> size() {
+        var fileSize = FileSystems.getDefault().getPath(Main.class.getPackageName() + ".size").toAbsolutePath();
+        if (!TS_FileUtils.isExistFile(fileSize)) {
+            var fileConsoleApp = FileSystems.getDefault().getPath(Main.class.getPackageName() + ".exe").toAbsolutePath();
+            if (TS_FileUtils.isExistFile(fileConsoleApp)) {
+                return TGS_Optional.ofEmpty("ERROR @", TS_GVMFixScreenSizeUtils.class.getSimpleName(), "size", "File not found", fileConsoleApp.toString());
+            }
+            var strSize = TS_OsProcess.of(fileConsoleApp.toString()).output;
+            if (TGS_StringUtils.isNullOrEmpty(strSize)) {
+                return TGS_Optional.ofEmpty("ERROR @", TS_GVMFixScreenSizeUtils.class.getSimpleName(), "size", "Cannot read fileSize", fileSize.toString());
+            }
+            TS_FileTxtUtils.toFile(strSize, fileSize, false);
+        }
+        if (!TS_FileUtils.isExistFile(fileSize)) {
+            return TGS_Optional.ofEmpty("ERROR @", TS_GVMFixScreenSizeUtils.class.getSimpleName(), "size", "Cannot write fileSize", fileSize.toString());
+        }
+        var strSize = TS_FileTxtUtils.toString(fileSize);
+        if (TGS_StringUtils.isNullOrEmpty(strSize)) {
+            return TGS_Optional.ofEmpty("ERROR @", TS_GVMFixScreenSizeUtils.class.getSimpleName(), "size", "File content is empty", fileSize.toString());
+        }
+        var valuesStr = TS_StringUtils.toList_spc(strSize);
+        if (valuesStr.size() != 4) {
+            return TGS_Optional.ofEmpty("ERROR @", TS_GVMFixScreenSizeUtils.class.getSimpleName(), "size", "File content values count is not 4", fileSize.toString());
+        }
+        var valuesInt = TGS_StreamUtils.toLst(valuesStr.stream().map(s -> TGS_CastUtils.toInteger(s)).filter(i -> i != null));
+        if (valuesInt.size() != 4) {
+            return TGS_Optional.ofEmpty("ERROR @", TS_GVMFixScreenSizeUtils.class.getSimpleName(), "size", "File content values that can be converted to integer count is not 4", fileSize.toString());
+        }
+        return TGS_Optional.of(new Rectangle(valuesInt.get(0), valuesInt.get(1), valuesInt.get(2), valuesInt.get(3)));
+    }
+}
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,3 +1,11 @@
 module com.tugalsan.gvm.screensize {
     requires java.desktop;
+    requires com.tugalsan.api.stream;
+    requires com.tugalsan.api.optional;
+    requires com.tugalsan.api.input;
+    requires com.tugalsan.api.cast;
+    requires com.tugalsan.api.os;
+    requires com.tugalsan.api.string;
+    requires com.tugalsan.api.file;
+    requires com.tugalsan.api.file.txt;
 }
